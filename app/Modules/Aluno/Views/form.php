2c104f77a4db181bc4302584be42f1c8fc7fe1ab
Daniel Frota <drfrota.adv@gmail.com>
2023-10-31 01:32:39 -0400

    crud de alunos ok

--- app/Modules/Aluno/Views/form.php
+++ app/Modules/Aluno/Views/form.php
@@ -1,6 +1,6 @@
 <?php echo $this->extend('_config/Layout/mainLayout'); ?>
 <?php echo $this->section('conteudo');
-  $a = $this->data[0];
+  $a = !empty($this->data) ? $this->data[0]: null;
 ?>
 <div class="md:mx-[210px] m-4">
     <p class=" text-3xl text-center font-semibold pb-4">Cadastro de Aluno</p>
@@ -9,19 +9,18 @@
 <form class="bg-white dark:bg-gray-900 p-8" method="post" action="<?= empty($a)? '/aluno/novo': "/aluno/editar/$a->id" ?>">
 <div class="mb-6">
     <label for="nome" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome do Aluno</label>
-    <input name="nome" value="<?= $a->nome ?>" type="text" id="nome" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+    <input name="nome" value="<?= empty($a)? '' : $a->nome ?>" type="text" id="nome" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
   </div>
 <div class="mb-6">
     <label for="matricula_suap" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Matrícula SUAP</label>
-    <input name="matricula_suap" value="<?= $a->matricula_suap ?>" type="text" id="matricula_suap" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+    <input name="matricula_suap" value="<?= empty($a)? '' : $a->matricula_suap ?>" type="text" id="matricula_suap" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
   </div>
   <div class="mb-6">
     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">E-mail</label>
-    <input name="email" value="<?= $a->email ?>" type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+    <input name="email" value="<?= empty($a)? '' : $a->email ?>" type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
   </div>
   <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Salvar</button>
 </form>
-
     </div>
 </div>
 <?php echo $this->endSection(); ?>
