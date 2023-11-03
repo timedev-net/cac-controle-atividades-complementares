67ac53e6a2ba763845de30f0ad0502d7db9c4718
Daniel Frota <drfrota.adv@gmail.com>
2023-11-03 14:45:25 -0400

    fix migrations e seeds

--- app/Database/Migrations/2023-10-29-162843_CreateAtividadesComplementares.php
+++ app/Database/Migrations/2023-10-29-162843_CreateAtividadesComplementares.php
@@ -11,19 +11,19 @@ class CreateAtividadesComplementares extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id' => ['type' => 'INT', 'constraint'     => 5, 'unsigned'       => true, 'auto_increment' => true],
-            'nome_atividade' => ['type' => 'VARCHAR', 'constraint' => '100', 'null' => false],
-            'aluno_id' => ['type' => 'INT', 'null' => false],
-            'tp_atividade_id' => ['type' => 'INT', 'null' => false],
-            'ano_letivo' => ['type' => 'INT', 'null' => false],
-            'periodo_letivo' => ['type' => 'INT', 'null' => false],
-            'data_inicio' => ['type' => 'DATE', 'null' => false],
-            'data_conclusao' => ['type' => 'DATE', 'null' => false],
-            'carga_horaria' => ['type' => 'INT', 'null' => false],
-            'obs_complementares' => ['type' => 'TEXT', 'null' => true],
-            'deferida' => ['type' => 'BOOLEAN', 'null' => true],
+            'id'                  => ['type' => 'INT', 'constraint'     => 5, 'unsigned'       => true, 'auto_increment' => true],
+            'nome_atividade'      => ['type' => 'VARCHAR', 'constraint' => '100', 'null' => false],
+            'aluno_id'            => ['type' => 'INT', 'null' => false],
+            'tp_atividade_id'     => ['type' => 'INT', 'null' => false],
+            'ano_letivo'          => ['type' => 'INT', 'null' => false],
+            'periodo_letivo'      => ['type' => 'INT', 'null' => false],
+            'data_inicio'         => ['type' => 'DATE', 'null' => false],
+            'data_conclusao'      => ['type' => 'DATE', 'null' => false],
+            'carga_horaria'       => ['type' => 'INT', 'null' => false],
+            'obs_complementares'  => ['type' => 'TEXT', 'null' => true],
+            'deferida'            => ['type' => 'BOOLEAN', 'null' => true],
             'razao_indeferimento' => ['type' => 'VARCHAR', 'constraint' => '200', 'null' => true],
-            'incluido_em' => ['type' => 'TIMESTAMP', 'default' => new RawSql('CURRENT_TIMESTAMP'),
+            'incluido_em'         => ['type' => 'TIMESTAMP', 'default' => new RawSql('CURRENT_TIMESTAMP'),
         ],
         ]);
         $this->forge->addPrimaryKey('id');
