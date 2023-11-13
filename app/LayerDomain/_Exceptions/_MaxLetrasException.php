05332d5aa4220d7bdbc7a1a1507559800e231b29
Daniel Frota <drfrota.adv@gmail.com>
2023-11-13 14:13:03 -0400

    fix exceções

--- app/LayerDomain/_Exceptions/_MaxLetrasException.php
+++ app/LayerDomain/_Exceptions/_MaxLetrasException.php
@@ -4,7 +4,8 @@ namespace App\LayerDomain\_Exceptions;
 use Exception;
 
 class _MaxLetrasException extends Exception {
-    public function __construct(int $max = 100, $message = "O Campo deve ter no máximo $max letras") {
+    public function __construct(int $max = 100, $message = "") {
+        $message = "O Campo deve ter no máximo $max letras";
         parent::__construct($message);
     }
 }
--- app/LayerDomain/_Exceptions/_MinLetrasException.php
+++ app/LayerDomain/_Exceptions/_MinLetrasException.php
@@ -4,7 +4,8 @@ namespace App\LayerDomain\_Exceptions;
 use Exception;
 
 class _MinLetrasException extends Exception {
-    public function __construct(int $min = 3, $message = "O Campo deve ter no mínimo $min letras") {
+    public function __construct(int $min = 3, $message = "") {
+        $message = "O Campo deve ter no mínimo $min letras";
         parent::__construct($message);
     }
 }
