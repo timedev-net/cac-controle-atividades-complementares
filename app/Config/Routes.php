f2b3deb3c22686b8a39f0138829974b881592c20
Daniel Frota <94360065272>
2024-01-13 09:26:51 -0400

    aplicação rodando

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,7 +7,7 @@ $routes->setDefaultNamespace('App\LayerExternal\Controllers');
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'HomeController::index');
 
 
 // ALUNOS
